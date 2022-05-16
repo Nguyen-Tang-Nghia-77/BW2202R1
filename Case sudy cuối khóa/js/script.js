1a819e8aaf5ad43bfa1638304dad330c7d5f07be
Nguyen-Tang-Nghia-77 <101049903+Nguyen-Tang-Nghia-77@users.noreply.github.com>
2022-05-16 18:53:27 +0700

    create new file
    create new file

---
+++ Case sudy cuối khóa/js/script.js
@@ -0,0 +1,38 @@
+let navbarDiv = document.querySelector('.navbar-travel');
+window.addEventListener('scroll', () => {
+    if(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40){
+        navbarDiv.classList.add('navbar-travel-cng');
+    } else {
+        navbarDiv.classList.remove('navbar-travel-cng');
+    }
+});
+
+
+const navbarCollapseDiv = document.getElementById('navbar-collapse-travel');
+const navbarShowBtn = document.getElementById('navbar-show-btn');
+const navbarCloseBtn = document.getElementById('navbar-close-btn');
+// show navbar
+navbarShowBtn.addEventListener('click', () => {
+    navbarCollapseDiv.classList.add('navbar-collapse-rmw');
+});
+
+// hide side bar
+navbarCloseBtn.addEventListener('click', () => {
+    navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target.id != "navbar-collapse-travel" && e.target.id != "navbar-show-btn" && e.target.parentElement.id != "navbar-show-btn"){
+        navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
+    }
+});
+
+// stop transition and animatino during window resizing
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove("resize-animation-stopper");
+    }, 400);
+});
